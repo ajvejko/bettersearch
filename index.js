ce6fbaabd99ec00146280894deec447f5e808a92
ajvejko <moucka.matej01@gmail.com>
2022-09-12 20:33:38 +0200

    improved search function and search happens in the same tab instead of opening new one

--- index.js
+++ index.js
@@ -1,12 +1,32 @@
-let lastbutton;
-let web = "";
+let web, term, lastbutton;
+const newLink = (term) =>{
+    const links =  
+    {
+        reddit:{
+            homeURL:"https://www.reddit.com",
+            searchURL:`https://www.google.com/search?q=${term} site:reddit.com`
+        },
+        stackoverflow:{
+            homeURL:"https://stackoverflow.com",
+            searchURL:`https://www.google.com/search?q=${term} site:stackoverflow.com`
+        },
+        youtube:{
+            homeURL:"https://youtube.com",
+            searchURL:`https://www.youtube.com/results?search_query=${term}`
+        },
+        github:{
+            homeURL:"https://github.com",
+            searchURL:`https://www.google.com/search?q=${term} site:github.com`
+        }
+    }
+    return links;
+}
 function keepColor(btn){
     const button = document.getElementById(btn);
     const borderColor = window.getComputedStyle(button).borderColor; 
-    web = button.id.toLocaleLowerCase();
-    console.log(web);
+    web = button.id;
     //Changes border color of last button if another pressed
-    if(lastbutton != null)  
+    if(lastbutton != null)
     lastbutton.style.borderColor = "rgb(255, 255, 255)";
     //Changes color properly
     borderColor === "rgb(255, 255, 255)" ? button.style.borderColor = "rgb(109, 138, 255)"  
@@ -23,19 +43,10 @@ function keepColor(btn){
 }
 
 function search() {
-    let search = document.getElementById("input-search").value;
-    //if field is empty AND website is selected
-    if(web === "youtube"){
-        window.open("https://www.youtube.com/results?search_query="+search);
-    } else {
-        //adds expression to limit results
-        search += ` site:${web}.com`;
-        window.open("https://google.com/search?q="+search);
-    }
-    //if no specific web is searched
-    if(web === ""){
-        search = document.getElementById("input-search").value;
-    }   
+    term = document.getElementById("input-search").value;
+    const searchterm = newLink(term);
+    term === "" ? window.open(searchterm[web].homeURL, "_self")
+    : window.open(searchterm[web].searchURL, "_self");
 }
 
 document.body.onkeydown = function(e){
