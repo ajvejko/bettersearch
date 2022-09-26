87c5ecc42e56447a8118e80bbd0a7a2bc8ad9388
ajvejko <moucka.matej01@gmail.com>
2022-09-26 22:15:55 +0200

    merged JS files

--- index.js
+++ index.js
@@ -1,19 +1,52 @@
 let links = JSON.parse(localStorage.getItem("links")) || [];
-//function to open the popup, 
-//also decides if the added button is shortcut or search
+let web, term, lastButton;
+const WHITE = "rgb(255, 255, 255)";
+const BLUE = "rgb(109, 138, 255)";
+//runs buildbuttons function on load
 window.onload = function(){
     links.forEach(buildbuttons);
 }
-
+//function for controlling the buttons colors
+function keepColor(btn){
+    const button = document.getElementById(btn);
+    const borderColor = window.getComputedStyle(button).borderColor;    
+    //Changes border color of last button if another pressed
+    if(lastButton)
+    lastButton.style.borderColor = WHITE;
+    //Changes color and placeholder text properly
+    if(borderColor === WHITE){
+        button.style.borderColor = BLUE;
+        document.getElementById("input-search").placeholder = `Searching through ${btn}`;
+        web = button.id;
+    } else {
+        button.style.borderColor = WHITE;
+        document.getElementById("input-search").placeholder = "Searching through everything";
+        web = undefined;
+    }
+    //store the previous button, used for border removal
+    lastButton = document.getElementById(btn);
+}
+//function to search
+function search(){
+    term = document.getElementById("input-search").value;
+    const searchTerm = newLink(term);
+    //if no button is selected, just google
+    if(!web)
+        return window.open(searchTerm.google.searchURL, "_self");
+    //if term is empty, search homeURL else use searchURL
+   !term ? window.open(searchTerm[web].homeURL, "_self")
+   : window.open(searchTerm[web].searchURL, "_self");
+}
+//builds buttons on load up
 function buildbuttons({name}){
     const addedButton = document.createElement("button");
     addedButton.innerText = name;
     addedButton.id = name;
     addedButton.className = "btn";
-    addedButton.onclick = "keepColor()";
+    addedButton.onClick = function(){keepColor(this.id)};
     document.getElementById("web").before(addedButton);
 }
-
+//opens the popup
 function addpopup(btn){
     const buttonType = document.getElementById(btn);
     let popupTitle = document.getElementById("div-popup-title");
@@ -27,6 +60,7 @@ function addpopup(btn){
     }
 }
 
+//adds new button
 function addbutton(btn){
     const buttonType = document.getElementById(btn);
     const name = document.getElementById("input-add-name").value;
@@ -46,4 +80,10 @@ function addbutton(btn){
     })
     localStorage.setItem("links", JSON.stringify(links));
     console.log(links);
+}
+
+document.body.onkeydown = function(e){
+    if(e.keyCode == 13){
+        search();
+    }
 }
